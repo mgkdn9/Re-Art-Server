80c00f62d7d7c49cac15fa3eced8784299397e4d
isaac8069 <isaac8069@gmail.com>
2021-12-13 11:41:04 -0500

    piece model created

---
+++ app/models/piece.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const pieceSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        artist: {
+            type: String,
+            required: true,
+        },
+        isAssigned: {
+            type: Boolean,
+            required: true,
+        },
+        tags: {
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: 'Tag',
+            required: true,
+        },
+        isPurchased: {
+            type: Boolean,
+            required: true
+        },
+    },
+
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('Piece', pieceSchema)
