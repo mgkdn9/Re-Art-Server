d6db4d2828a4e4e7e5aa3eaad479c0837703066b
Mike Kohlberg <mkohlberg95@gmail.com>
2025-07-25 17:04:55 -0600

    simpler db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,27 @@
-'use strict'
+require('dotenv').config()
 
-// creating a base name for the mongodb
-// REPLACE THE STRING WITH YOUR OWN DATABASE NAME
-const mongooseBaseName = 'exampleApp'
+const db = process.env.MONGODB_URI
+module.exports = db
 
-// create the mongodb uri for development and test
-const database = {
-	development: `mongodb://localhost/${mongooseBaseName}-development`,
-	test: `mongodb://localhost/${mongooseBaseName}-test`,
-}
 
-// Identify if development environment is test or development
-// select DB based on whether a test file was executed before `server.js`
-const localDb = process.env.TESTENV ? database.test : database.development
+// 'use strict'
 
-// Environment variable MONGODB_URI will be available in
-// heroku production evironment otherwise use test or development db
-const currentDb = process.env.MONGODB_URI || localDb
+// // creating a base name for the mongodb
+// // REPLACE THE STRING WITH YOUR OWN DATABASE NAME
+// const mongooseBaseName = 'exampleApp'
 
-module.exports = currentDb
+// // create the mongodb uri for development and test
+// const database = {
+// 	development: `mongodb://localhost/${mongooseBaseName}-development`,
+// 	test: `mongodb://localhost/${mongooseBaseName}-test`,
+// }
+
+// // Identify if development environment is test or development
+// // select DB based on whether a test file was executed before `server.js`
+// const localDb = process.env.TESTENV ? database.test : database.development
+
+// // Environment variable MONGODB_URI will be available in
+// // heroku production evironment otherwise use test or development db
+// const currentDb = process.env.MONGODB_URI || localDb
+
+// module.exports = currentDb
