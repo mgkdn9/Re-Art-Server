31a76e983ffd9177ecc38fe795a11953791ad891
Mike Kohlberg <mkohlberg95@gmail.com>
2025-07-25 16:40:31 -0600

    cors correction

--- server.js
+++ server.js
@@ -50,9 +50,14 @@ const app = express()
 
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(
-	cors()
-)
+// app.use(
+// 	cors()
+// )
+app.use(cors({
+  origin: 'https://mgkdn9.github.io', // Allow GitHub Pages frontend
+  credentials: true, // Optional: if you use cookies/auth
+}));
+
 
 // define port for API to run on
 // adding PORT= to your env file will be necessary for deployment
